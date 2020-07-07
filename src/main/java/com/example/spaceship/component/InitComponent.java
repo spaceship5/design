25936236600d21c14a86d1e7c07b9749ccb258f5
ZHangPeng1999 <2074809357@qq.com>
2020-07-07 09:45:23 +0800

    initComponent 完成

--- src/main/java/com/example/spaceship/component/InitComponent.java
+++ src/main/java/com/example/spaceship/component/InitComponent.java
@@ -1,5 +1,8 @@
 package com.example.spaceship.component;
 
+import com.example.spaceship.entity.Teacher;
+import com.example.spaceship.entity.User;
+import com.example.spaceship.service.UserService;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,19 +13,19 @@ import org.springframework.stereotype.Component;
 public class InitComponent implements InitializingBean {
     @Autowired
     private PasswordEncoder encoder;
-//    @Autowired
-//    private UserService userService;
+    @Autowired
+    private UserService userService;
     @Override
     public void afterPropertiesSet() throws Exception {
-//        int num=1001;
-//        if(userService.getUser(num)==null){
-//            User user=new User();
-//            user.setNumber(num);
-//            user.setName("root");
-//            user.setRole(User.Role.ADMIN);
-//            Teacher tea=new Teacher();
-//            tea.setPassword(encoder.encode("root"));
-//            userService.addTeacher(tea, user);
-//        }
+        int num=1001;
+        if(userService.getUserByNum(num)==null){
+            User user=new User();
+            user.setNumber(num);
+            user.setName("root");
+            user.setRole(User.Role.ADMIN);
+            Teacher tea=new Teacher();
+            user.setPassword(encoder.encode("root"));
+            userService.addTeacher(tea, user);
+        }
     }
 }
--- src/main/java/com/example/spaceship/service/UserService.java
+++ src/main/java/com/example/spaceship/service/UserService.java
@@ -1,15 +1,27 @@
 package com.example.spaceship.service;
 
+import com.example.spaceship.entity.Teacher;
 import com.example.spaceship.entity.User;
+import com.example.spaceship.repository.TeacherRepository;
 import com.example.spaceship.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private TeacherRepository teacherRepository;
     public User getUserByNum(Integer number) {
        return userRepository.find(number);
+    }
+
+    public Teacher addTeacher(Teacher tea, User user) {
+        tea.setUser(user);
+        teacherRepository.saveAndFlush(tea);
+        teacherRepository.refresh(tea);
+        return tea;
     }
 }
